93dbd918426512a8c8525c0c27eed9b7a8e7eb02
Mill Nonthakorn <34181310+UsagiLiw@users.noreply.github.com>
2021-06-27 02:24:19 +0700

    seeded database *only first level category*

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'net/http'
+require 'json'
+
+Category.destroy_all
+
+url = 'https://s3-ap-southeast-1.amazonaws.com/kiyo-development/test/categories.json'
+uri = URI(url)
+response = Net::HTTP.get(uri)
+all_categories = JSON.parse(response) 
+
+all_categories.each do |category|
+    Category.create(
+        {
+            catId: category["id"],
+            name: category["name"],
+            label: category["label"],
+            isLeaf: category["isLeaf"],
+            firstLevelCatId: category["firstLevelCatId"],
+            variationCat: category["variationCat"],
+            active: category["active"],
+        }
+    )
+end
+
+puts "It has been seeded."
